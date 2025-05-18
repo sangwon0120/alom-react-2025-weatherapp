44b646a0335e13f208c2bc7305d9b29bc0e1f1cc
변상원 <sangwonbyun7@gmail.com>
2025-05-18 22:00:52 +0900

    일주일 날짜 정보보

--- src/components/DailyForecast.js
+++ src/components/DailyForecast.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { DailyForecastWrapper, DailyItem } from "./styles/StyledComponents";
+import { DailyForecastWrapper, DailyItem} from "./styles/StyledComponents";
 import { getWeatherDescription, formatDailyData } from "../utils/weather";
 
 const DailyForecast = ({ weatherData }) => {
   const dailyData = formatDailyData(weatherData);
-
-  return <div>채워주세요</div>;
+  const krDays =['일','월','화','수','목','금','토'];
+  return (
+    <DailyForecastWrapper>
+      {dailyData.map((item,i) => (
+        <DailyItem key={item.date}>
+          <span>
+            {item.date.slice(5,7)}월 
+            {' '+item.date.slice(8,10)}일  
+            {" (" + krDays[i] + ")"}
+            </span>
+          <span>{item.description}</span>
+          <span>{item.maxTemp}°C</span>
+        </DailyItem>
+      ))}
+    </DailyForecastWrapper>
+  );
 };
 
 export default DailyForecast;
