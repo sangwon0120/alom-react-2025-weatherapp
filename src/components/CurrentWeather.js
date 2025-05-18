6f01a630c527e8692b34fd9795cbe17ef31dea89
변상원 <sangwonbyun7@gmail.com>
2025-05-18 22:00:41 +0900

    현재 날씨 정보

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,14 +4,20 @@ import {
   Temperature,
   WeatherCode,
 } from "./styles/StyledComponents";
-import { getWeatherDescription } from "../utils/weather";
+import { formatHourlyData, getWeatherDescription } from "../utils/weather";
 
 const CurrentWeather = ({ weatherData, isLoading }) => {
   if (isLoading) {
-    return <div>채워주세요</div>;
+    return <div>
+      <h1>로딩중...</h1>
+    </div>;
   }
-
-  return <div>채워주세요</div>;
+  const hourly = formatHourlyData(weatherData);
+  const current = hourly[0];
+  return <CurrentWeatherWrapper>
+    <Temperature>{current.temp}°C</Temperature>
+    <WeatherCode>{current.description}</WeatherCode>
+  </CurrentWeatherWrapper>
 };
 
 export default CurrentWeather;
